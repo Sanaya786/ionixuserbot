2721c75053b7477d8519e2edc1631b57b287b3f4
brokendevil07 <67486105+brokendevil07@users.noreply.github.com>
2020-09-28 00:07:16 +0530

    𝐈𝐎𝐍𝐈𝐗™️

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -5,10 +5,10 @@ from datetime import datetime
 @borg.on(admin_cmd(pattern="ping$"))
 @borg.on(sudo_cmd(pattern="ping$", allow_sudo=True))
 async def _(event):
-    starkislub = await edit_or_reply(event, "`Pong !`")
+    starkislub = await edit_or_reply(event, "`📌Pong !`")
     if event.fwd_from:
         return
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await starkislub.edit("**Pong!**\n`{}`\n For Assistance Join @FridayOT".format(ms))
+    await starkislub.edit("**Pong!**\n`{}`\n any query Join @ionix_updates".format(ms))
