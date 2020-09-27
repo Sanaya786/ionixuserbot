f6da1650978af9af8536f4b35583a0c26f18226d
brokendevil07 <67486105+brokendevil07@users.noreply.github.com>
2020-09-27 12:30:07 +0530

    𝐈𝐎𝐍𝐈𝐗™️

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -41,7 +41,7 @@ NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play desp
 NO_SQL = "`Running on Non-SQL mode!`"
 
 CHAT_PP_CHANGED = "`Chat Picture Changed`"
-CHAT_PP_ERROR = "`Some issue with updating the pic,`" \
+CHAT_PP_ERROR = "`Some issue with updating  the pic,`" \
                 "`maybe coz I'm not an admin,`" \
                 "`or don't have enough rights.`"
 INVALID_MEDIA = "`Invalid Extension`"
